8ac5adb7991ab491243abb6198781e77f4e1a473
Minight <seandotyeoh@gmail.com>
2013-01-22 10:21:53 +1100

    trying to get the function to return an array

--- stuff.js
+++ stuff.js
@@ -7,31 +7,31 @@ function stripSpace(url){
     url = url.replace("%20", "");
     return url;
 }
-function getFile(url, type){
+// function processArray() YOU"RE UP TO HERE
+function getFile(url, type, index1){
+    csvArray = [];
     switch(type){
         case 'online':
             request(url, function (error, response, body) {
                 if (error) throw error;
-               // csv().from(body).to(fs.createWriteStream("./output/" +stripSpace(url)));
-                //console.log('loggedonline');
+                // csv().from(body).to(fs.createWriteStream("./output/" +stripSpace(url)));
+                csv().from(body).on('record', function(data, index){
+                    var index1 = [index, data];
+                    csvArray.push(index1);    
+                });
+                csv().from(body).on('end', function(data, index){
+                    // console.log(csvArray);
+                    return csvArray;
+                });
             });
             break;
         case 'local':
-            //csv().from(url).to(fs.createWriteStream("./output/" + url));
-                //console.log('logged offline');
             break;
         default:
             console.log('somethng went wrong' + url + " " + type);
     }
 }
-function addToFile(array, colName){
-    for(i = 0; i < array.length; i++){
-        var row = [];
-        row[columns.indexOf(colName)] = array[i];
-        finalCsv.push(row);
-    }
-}
-
+///////////////////////////
 
 var name = [    "http://syminight.com/OBS/SEM%20report.csv",
                 "http://syminight.com/OBS/conversion%20funnel.csv",
@@ -46,8 +46,9 @@ var type = [    "online",
                 "online",
                 "local"];
 var fileNames = [name, type];
-for(i = 0; i < name.length; i++){
-    getFile(fileNames[0][i],fileNames[1][i]);
+for(i = 0; i < 1; i++){
+    var csvReturn = [i];
+    csvReturn[i] = getFile(fileNames[0][i],fileNames[1][i], i);
 }
 
 var finalCsv = [];
@@ -62,7 +63,7 @@ var columns = [ " ",
                 "ytd"];
 finalCsv.push(columns);
 //the item column *pardon the longass array
-var items = [   "Visits", "   Paid Clicks", "   Unpaid Clicks", "Switches", "   Paid Switches", "   Unpaid Switches", "Conversion Rate", "   Paid", "   Unpaid", "eGPPS", "Gross Profit", "", "Impressions", "Clicks", "CTR", "AVG CPC", "Switches", "ConvRate", "Adj Switches", "Ad Spend", "CPA", "Total Spend", "eCPA", "", "Unique Visitors", "Page Views", "Avg. Duration", "Bounce Rate", "", "Paid Search", "Organic Search", "Email", "Direct", "Referral", "", "Alinta Energy", "Click Energy", "Energy Australia", "Australia Power & Gas", "", "NSW", "QLD", "SA", "VIC"];
+/*var items = [   "Visits", "   Paid Clicks", "   Unpaid Clicks", "Switches", "   Paid Switches", "   Unpaid Switches", "Conversion Rate", "   Paid", "   Unpaid", "eGPPS", "Gross Profit", "", "Impressions", "Clicks", "CTR", "AVG CPC", "Switches", "ConvRate", "Adj Switches", "Ad Spend", "CPA", "Total Spend", "eCPA", "", "Unique Visitors", "Page Views", "Avg. Duration", "Bounce Rate", "", "Paid Search", "Organic Search", "Email", "Direct", "Referral", "", "Alinta Energy", "Click Energy", "Energy Australia", "Australia Power & Gas", "", "NSW", "QLD", "SA", "VIC"];
 var title = [' '];
     title[0] = "Site Stats";
     title[14] = "SEM Summary";
@@ -70,6 +71,6 @@ var title = [' '];
     title[30] = "Traffic Sources";
     title[37] = "Switches By";
 addToFile(title, " ");
-addToFile(items, "item");
+addToFile(items, "item");*/
 
 csv().from(finalCsv).to('output.csv');
