2d0088609755c5b018baf91d74f494da9087327f
Minight <seandotyeoh@gmail.com>
2013-01-21 20:46:06 +1100

    okay

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 var request = require("request");
-request("http://syminight.com/OBS/site%20summary.csv", function (error, response) {
+request("http://syminight.com/OBS/site%20summary.csv", function (error, response, body) {
 if (error) throw error;
-	console.log(response.data) 
+	console.log(body); 
 });
