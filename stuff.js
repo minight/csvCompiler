d56eb43443afb8f47c94b4ad41be0bb73ad9ed44
Minight <seandotyeoh@gmail.com>
2013-01-21 21:05:02 +1100

    update

--- stuff.js
+++ stuff.js
@@ -1,3 +1,4 @@
+var request = require("request");
 var csv = require('csv');
 csv().from.stream(request("http://syminight.com/OBS/site%20summary.csv")).on('record', function (data, index) {
 console.log('#'+index+' '+JSON.stringify(data));
