f6ea5c0bf11213d05a8a724d6824c75233ac9f3d
Minight <seandotyeoh@gmail.com>
2013-01-23 11:24:18 +1100

    tidy'd up a bit... added more comments... starting on the calculations. havent started on api at this point

--- stuff.js
+++ stuff.js
@@ -2,52 +2,53 @@
 var request = require("request");
 var csv = require("csv");
 var fs = require("fs");
-    
-function stripSpace(url){
-    url = url.substr(25);
-    url = url.replace("%20", "");
-    return url;
+var url = require("url");
+
+function decodeUrl(link){
+    var decode = decodeURIComponent(url.parse(link).pathname);
+    decode = decode.slice(decode.lastIndexOf("/") + 1);
+    decode = decode.slice(0, decode.lastIndexOf("."));
+    return decode;
 }
 
-function processArray(array, column){
+function processArray(array, index, column){
 //return 4;
 //chosen by diceroll, guaranteed to be random
     for(i = 0; i < items.length; i++){
         var itemName = items[i];
         var calcResult;
         switch(itemName){
-            
+
         }
     }
 }
-
-function getFile(url, type, iterator, callback){
-// takes the url, reads the csv, and adds it to an array.
-    var csvArray = []; 
+//used in the getFile function. To make it easier to read
+//in turn created a realllly long callback chain
+function pushToArray(location, url, callback){
+    var csvArray = [];
     var container = [];
+    csv().from(location).on('record', function(data, index){
+        var container = data;
+        csvArray.push(container);
+    });
+    csv().from(location).on('end', function(data, index){
+        callback(csvArray, decodeUrl(url));
+    });
+}
+// takes the url, reads the csv, and adds it to an array
+function getFile(url, type, iterator, callback){
     switch(type){
-        case 'online':
-            request(url, function (error, response, body) {
-                if (error) throw error;
-                // csv().from(body).to(fs.createWriteStream("./output/" +stripSpace(url)));
-                csv().from(body).on('record', function(data, index){
-                    var container = data;
-                    csvArray.push(container);
-                });
-                csv().from(body).on('end', function(data, index){
-                    callback(csvArray);
-                    fs.writeFile("result.txt", csvArray, function(err){
-                        if (err) throw err;
-                        // console.log("Its Saved... Hopefully");
-                    });
-                });
-            });
-            break;
-
-        case 'local':
-            break;
-        default:
-            console.log('somethng went wrong' + url + " " + type);
+    case 'online':
+        request(url, function (error, response, body) {
+            if (error) throw error;
+            pushToArray(body, url, callback);
+        });
+        break;
+    case 'local':
+        pushToArray(url, url, callback);
+        break;
+    default:
+        console.log('somethng went wrong' + url + " " + type);
     }
 }
 //addToFile used to intialize the finalCsv array, so you dont get undefined rows
@@ -56,7 +57,6 @@ function addToFile(fnArray, fnColumn){
         var row = [];
         row[columns.indexOf(fnColumn)] = fnArray[i];
         finalCsv.push(row);
-        // console.log(fnArray[i] + " index: " + i);
     }
 }
 //used to fill data in columns, will fill as is in the array, so inlcude any gaps
@@ -66,66 +66,69 @@ function addColumn(fnArray, fnColumn){
         if(finalCsv[0][fnCounter] == fnColumn){
             break;
         }else{
-            // console.log(finalCsv[0][fnCounter]);
-            fnCounter++;
+             fnCounter++;
         }
     }  
-    // console.log(fnCounter); 
     for(i = 0; i < fnArray.length; i++){
-        // console.log("value: " + fnArray[i] + " iterator: " + i);
         finalCsv[i+1][fnCounter] = fnArray[i];
     }
 }
 /////////////////////////// end function declaration
 
+//LOCATION OF FILES, UNLESS USING GOOGLE API WHICH I WILL EVENTUALLY.
 var name = [    "http://syminight.com/OBS/SEM%20report.csv",
                 "http://syminight.com/OBS/conversion%20funnel.csv",
                 "http://syminight.com/OBS/site%20summary.csv",
                 "http://syminight.com/OBS/source%20report.csv",
                 "http://syminight.com/OBS/switch%20report.csv",
                 "./rates.csv"];
+//WHETHER THE FILE IS LOCAL OR IS ONLINE, DEFINES HOW ITS TREATED WHEN READING DATA
 var type = [    "online",
                 "online",
                 "online",
                 "online",
                 "online",
                 "local"];
-var fileNames = [name, type];
-outputArray = [];
-for(i = 0; i < name.length; i++){
-    var getFileCounter = 0;
-    getFile(fileNames[0][i],fileNames[1][i], i, function(array){
-        // console.log("\nyes\n"  + array);
-        getFileCounter++;
-        outputArray.push(array);
-        console.log(getFileCounter);
-        if(getFileCounter >= 5){
-            console.log(outputArray);
-        }
-    });
-}
-
-var finalCsv = [];
 //intializing all the static stuff
 var columns = [ " ",
-                "item", 
-                "prevDate", 
+                "item",
+                "prevDate",
                 "prevPrevDate",
                 "change",
                 "vtb",
                 "wtd",
                 "ytd"];
-finalCsv.push(columns);
 //the item column *pardon the longass array (second column)
 var items = [   "Visits", "   Paid Clicks", "   Unpaid Clicks", "Switches", "   Paid Switches", "   Unpaid Switches", "Conversion Rate", "   Paid", "   Unpaid", "eGPPS", "Gross Profit", "", "Impressions", "Clicks", "CTR", "AVG CPC", "Switches", "ConvRate", "Adj Switches", "Ad Spend", "CPA", "Total Spend", "eCPA", "", "Unique Visitors", "Page Views", "Avg. Duration", "Bounce Rate", "", "Paid Search", "Organic Search", "Email", "Direct", "Referral", "", "Alinta Energy", "Click Energy", "Energy Australia", "Australia Power & Gas", "", "NSW", "QLD", "SA", "VIC"];
 //title column for the first one
-var title = [' '];
+var title = [];
     title[0] = "Site Stats";
     title[12] = "SEM Summary";
     title[24] = "Site Summary";
     title[29] = "Traffic Sources";
     title[35] = "Switches By";
     title[43] = "";
+var fileNames = [name, type];
+var outputArray = [];
+var outputArrayIndex = [];
+var finalCsv = [];
+
+for(i = 0; i < name.length; i++){
+    var getFileCounter = 0;
+    getFile(fileNames[0][i],fileNames[1][i], i, function(array, name){
+        getFileCounter++;
+        outputArrayIndex.push(name);
+        outputArray.push(array);
+        if(getFileCounter > 5){
+            // note to future suicidal programmers
+            // the outputArray stored is in the following structure
+            // [ [ [REPORT1 ROW 1], [REPORT1 ROW 2] ], [ [REPORT2 ROW 1], [REPORT2} ROW 2] ] ]
+            // the order they are stored in is chosen by diceroll, so use outputArrayIndex for the index
+            
+        }
+    }); // I MADE A FUNCTION IN A LOOP. WHAT ARE YOU GOING TO DO ABOUT IT
+}
+finalCsv.push(columns);
 addToFile(title, " "); //using this different function to create all the rows to prevent type errors
 addColumn(items, "item"); //all the others will use this function to append to the created rows
 
